9f9cd63fc1617874a0d8729b050afe7f5b95a7a0
Gustavo Henrique Braga Fernandes <gutilokossj12@gmail.com>
2024-08-30 10:22:27 -0300

    Ajustando para testes

--- docs/script.js
+++ docs/script.js
@@ -11,3 +11,33 @@ document.getElementById('consultForm').addEventListener('submit', function(event
         document.getElementById('errorMessage').textContent = 'CNPJ inválido. Verifique e tente novamente.';
     }
 });
+
+function redirectToPage(cnpj, targetPage) {
+    const cleanedCNPJ = cnpj.replace(/\D/g, ''); // Remove tudo que não for número
+
+    if (cleanedCNPJ.length === 14 || cleanedCNPJ.length === 18) {
+        // Redireciona para a página de resultado passando o CNPJ via URL
+        window.location.href = `${targetPage}?cnpj=${cleanedCNPJ}`;
+    } else {
+        document.getElementById('errorMessage').textContent = 'CNPJ inválido. Verifique e tente novamente.';
+    }
+}
+
+// Event listener para o botão "Consultar Módulos"
+document.getElementById('consultarModulosBtn').addEventListener('click', function() {
+    const cnpj = document.getElementById('cnpj').value;
+    redirectToPage(cnpj, 'resultado.html');
+});
+
+// Event listener para o botão "Consultar Backup"
+document.getElementById('consultarBackupBtn').addEventListener('click', function() {
+    const cnpj = document.getElementById('cnpj').value;
+    redirectToPage(cnpj, 'resultado_BKP.html');
+});
+
+// Event listener para o formulário (mantém a compatibilidade com o submit do formulário)
+document.getElementById('consultForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const cnpj = document.getElementById('cnpj').value;
+    redirectToPage(cnpj, 'resultado.html'); // Pode alterar para um dos botões se necessário
+});
