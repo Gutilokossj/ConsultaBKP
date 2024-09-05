7dfb66c37b36ec8903adfd6d7a46f751f5460c99
Gutilokossj <gutilokossj12@gmail.com>
2024-09-05 18:04:10 -0300

    Atualização automático da página a cada 1 minuto adicionado

--- docs/footer.js
+++ docs/footer.js
@@ -1,5 +1,5 @@
 document.getElementById('footer-placeholder').innerHTML = `
     <div class="container_footer">
-        <h3>Soften Sistemas LTDA - ver. 1.1.5 - by Gustavo Fernandes</h3>
+        <h3>Soften Sistemas LTDA - ver. 1.1.6 - by Gustavo Fernandes</h3>
     </div>
 `;
