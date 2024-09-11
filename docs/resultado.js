c28ce3094aa1b5a5a018bb007d032b4b347368ab
Gustavo Henrique Braga Fernandes <gutilokossj12@gmail.com>
2024-09-11 12:59:48 -0300

    Subindo branch teste1

--- docs/resultado.js
+++ docs/resultado.js
@@ -5,17 +5,17 @@ window.addEventListener('DOMContentLoaded', async (event) => {
     if (cnpj) {
         try {
             const apiUrl = `https://servidor-proxy.vercel.app/proxy/consulta/${cnpj}`;
-            console.log('Consultando API:', apiUrl); // Verifica a URL da API
+            console.log('Consultando API BKP:', apiUrl); // Verifica a URL da API
 
             const response = await fetch(apiUrl);
-            console.log('Resposta da API:', response); // Verifica o status da resposta
+            console.log('Resposta da API BKP:', response); // Verifica o status da resposta
 
             if (!response.ok) {
-                throw new Error('Erro ao consultar a API');
+                throw new Error('Erro ao consultar a API BKP');
             }
 
             const data = await response.json();
-            console.log('Dados recebidos da API:', data); // Verifica os dados recebidos
+            console.log('Dados recebidos da API BKP:', data); // Verifica os dados recebidos
 
             if (data.mensagem === 'Empresa não localizada') {
                 window.location.href = 'erro.html';
@@ -25,7 +25,6 @@ window.addEventListener('DOMContentLoaded', async (event) => {
                 document.getElementById('cnpj').textContent = formatCNPJ(data.cnpjCpfString) || 'Não disponível';
                 document.getElementById('ultimoEnvio').textContent = `${data.ultimoEnvioContador ? new Date(data.ultimoEnvioContador).toLocaleString('pt-BR') : 'Não configurado'}`;
                 document.getElementById('ultimoBackup').textContent = `${data.ultimoBackupBd ? new Date(data.ultimoBackupBd).toLocaleString('pt-BR') : 'Não disponível'}`;
-                document.getElementById('diasSemBKP');
 
                 // Atualize o campo de dias sem backup
                 if (data.ultimoBackupBd) {
@@ -39,9 +38,55 @@ window.addEventListener('DOMContentLoaded', async (event) => {
                 if (data.ultimoBackupBd) {
                     updateBackupStatus(data.ultimoBackupBd);
                 }
+                // Consultar a segunda API (POST) pelo proxy
+                const apiUrl2 = `https://servidor-proxy.vercel.app/proxy/release/${cnpj}`;
+                console.log('Consultando API 2:', apiUrl2); // Verifica a URL da API
+
+                const postData = {
+                    document: cnpj,
+                    origin: 'GerencieAqui',
+                    token: '0000000000972276318728136634554015351040452035258642456166472325640972276318728136634521661441618814104321228832361628209722763187281366345000000000000000000000000000000183728947554015351040452035259722763187281366345'
+                };
+
+                const response2 = await fetch(apiUrl2, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(postData)
+                });
+
+                console.log('Resposta da API 2:', response2); // Verifica o status da resposta
+
+                if (!response2.ok) {
+                    throw new Error('Erro ao consultar a API 2');
+                }
+
+                const data2 = await response2.json();
+                console.log('Dados recebidos da API 2:', data2); // Verifica os dados recebidos
+
+                // Atualize os módulos no HTML com base na resposta da segunda API
+                const moduleElements = {
+                    estoque: document.getElementById('moduloEstoque'),
+                    financeiro: document.getElementById('moduloFinanceiro'),
+                    comercial: document.getElementById('moduloComercial')
+                };
+
+                // Inicialize todos os módulos como 'Não disponível'
+                Object.keys(moduleElements).forEach(key => {
+                    moduleElements[key].textContent = 'Não disponível';
+                });
+
+                // Atualize os módulos com base nos dados recebidos
+                data2.benefits.forEach(benefit => {
+                    if (benefit.type === 'MODULO' && moduleElements[benefit.name]) {
+                        moduleElements[benefit.name].textContent = 'Disponível';
+                    }
+                });
             }
+            
         } catch (error) {
-            console.error('Erro ao consultar a API:', error);
+            console.error('Erro ao consultar as APIs:', error);
             window.location.href = 'erro.html';
         }
     } else {
